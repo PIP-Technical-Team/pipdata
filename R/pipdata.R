72daca51970eecf94ab80958adb25c71c15c3728
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-06-12 13:32:06 -0400

    document

--- R/pipdata.R
+++ R/pipdata.R
@@ -7,8 +7,8 @@
 #'
 #' @docType package
 #' @name pipdata
-#' @importFrom pipload add_gls_to_env
-#' @import data.table
+#' @rawNamespace import(collapse, except = fdroplevels)
+#' @rawNamespace import(data.table, except = fdroplevels)
 #' @importFrom glue glue
 
 # Make sure data.table knows we know we're using it
@@ -16,8 +16,6 @@
 
 # Prevent R CMD check from complaining about the use of pipe expressions
 # standard data.table variables
-if (getRversion() >= "2.15.1")
-  utils::globalVariables(c(".", ".I", ".N", ".SD"), utils::packageName())
 
 if (getRversion() >= "2.15.1") {
   utils::globalVariables(
