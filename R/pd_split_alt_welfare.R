49d777857a754fd462f1d61c92bfef1d0da5e32c
Diana Garcia <dgarciarojas@worldbank.org>
2025-06-24 08:51:45 -0400

    Fix to pd_split_alt_welfare to add welfare_type to attributes

--- R/pd_split_alt_welfare.R
+++ R/pd_split_alt_welfare.R
@@ -3,7 +3,7 @@
 #' Split data into two dataframes when there is alternative welfare in the same
 #' survey
 #'
-#' @param df data frame with group data, loaded with `pipload::pip_load_dlw()`
+#' @param dt data table loaded with `pipload::pip_load_dlw()`
 #' @param cpfw data frame with Price framework data for country/survey in `df`.
 #'   It is loaded with `get_country_pfw(df, pfw)`. `pfw` is loaded in
 #'   `pipload::pip_load_aux("pfw")`
@@ -16,21 +16,23 @@
 #' pfw  <- pipload::pip_load_aux("pfw")
 #' cpfw <- get_country_pfw(md, pfw)
 #' df   <- pd_split_alt_welfare(md, cpfw)
-pd_split_alt_welfare <- function(df, cpfw) {
+pd_split_alt_welfare <- function(dt, cpfw) {
 
   # Computations -------
 
   welf_type <- cpfw[[1]]$wt
 
-  df[,
-     welfare_type := welf_type
-      ]
+  # df[,
+  #    welfare_type := welf_type
+  #     ]
+  attr(dt, "welfare_type") <- welf_type
+
 
   ## One data frame ------
 
   if (length(cpfw)== 1) {
 
-    l <- list(df)
+    l <- list(dt)
     names(l) <- cpfw[[1]]$cache_id
 
     return(l)
@@ -42,12 +44,16 @@ pd_split_alt_welfare <- function(df, cpfw) {
   other_welfare      <- cpfw[[2]]$oth_welfare1_var #alternative wlf is in position 2
   other_welfare_type <- cpfw[[2]]$wt
 
-  dfa <- copy(df)
-  dfa[,
-      `:=`(
-        welfare = get(other_welfare),
-        welfare_type = other_welfare_type
-      )]
+  dta <- copy(dt)
+
+  dta[,
+      # `:=`(
+      #   welfare = get(other_welfare),
+      #   welfare_type = other_welfare_type )
+      welfare = get(other_welfare)
+      ]
+
+  attr(dta, "welfare_type") <- other_welfare_type
 
   l <- list(df, dfa)
 
