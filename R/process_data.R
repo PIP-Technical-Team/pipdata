77e95c129c1a3c16093be230e027d7a7cff4269d
Diana Garcia <dgarciarojas@worldbank.org>
2025-07-24 10:33:22 -0400

    Eliminate S3 method for process_data

--- R/process_data.R
+++ R/process_data.R
@@ -1,9 +1,6 @@
-#' PD: process data. Source: datalibweb
+#' Process datalibweb data: merge PFW data and clean variables
 #'
-#' process datalibweb data. Use S3 method to identify whether the data is
-#' microdata, groupdata or imputed data
-#'
-#' @param df dataframe loaded with `pipload::pip_load_dlw()`
+#' @param df dataframe loaded with `valid_dlw_load`
 #' @param pfw PFW
 #' @param ...  other parameters
 #'
@@ -24,16 +21,6 @@
 #' md  <- pipdata:::m_svy_id_to_att(md)
 #' process_data(md, pfw)
 process_data <- function(df, pfw, ...) {
-  UseMethod("process_data")
-
-}
-
-#' @param pfw dataframe with Price Framework data loaded with
-#'   `pipload::pip_load_aux("pfw")`
-#'
-#' @export
-#' @rdname process_data
-process_data.pipmd <- function(df, pfw, ...) {
 
   # on.exit ------------
   # on.exit({
@@ -90,92 +77,8 @@ process_data.pipmd <- function(df, pfw, ...) {
 
       NULL
 
-      }
-  )
-
-
-  # Return -------------
-  return(res)
-
-}
-
-#' @param pfw dataframe with Price Framework data loaded with
-#'   `pipload::pip_load_aux("pfw")`
-#'
-#' @export
-#' @rdname process_data
-process_data.pipgd <- function(df, pfw, ...) {
-
-  # on.exit ------------
-  # on.exit({
-  #   rm(survey_id,
-  #      envir = .pipdataenv)
-  # })
-
-  svy <- attributes(df)$survey_id
-
-  assign("survey_id",
-         svy,
-         envir = .pipdataenv)
-
-  if("countrycode" %in% names(df)){
-    df$country_code <- df$countrycode
-  }
-
-  # Computations -------
-  res <- tryCatch(
-    expr = {
-
-      ls_cpfw <- pd_cpfw_merge(df, pfw)
-
-      pd_dlw_clean(ls_cpfw)
-
-    },
-
-    piperr = function(cnd){
-
-      survey_id <- c(.pipdataenv$survey_id)
-
-      pipfun::log_add(event = "error",
-                      message = cnd$message,
-                      name = "pipdata_log",
-                      .trace = cnd$call,
-                      logmeta = list(error = class(cnd)[2],
-                                  survey = survey_id,
-                                  status = "The survey was skipped"))
-
-      NULL
-
-    },
-
-    error = function(cnd){
-
-      survey_id <- c(.pipdataenv$survey_id)
-
-      pipfun::log_add(event = "error",
-                      message = cnd$message,
-                      name = "pipdata_log",
-                      .trace = cnd$call,
-                      logmeta = list(error = "unknown_error",
-                                     survey = survey_id,
-                                     status = "The survey was skipped"))
-
-      NULL
-
     }
   )
 
-  # Return -------------
-  return(res)
-
-}
-
-
-#' @export
-#' @rdname process_data
-process_data.default <- function(df, ...) {
-
-  cli::cli_alert("no PIP method for this data. Returning NULL")
-  return(NULL)
 
 }
