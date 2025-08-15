d090b98dde17f473ea92a3ba363437fbe9e7c689
Diana Garcia <dgarciarojas@worldbank.org>
2025-08-15 08:38:11 -0400

    Create two inventories with update_pip_inventory

--- R/update_pip_inventory.R
+++ R/update_pip_inventory.R
@@ -7,13 +7,17 @@ update_pip_inventory <- function(inv_to_clean,
   # computations   ---------
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  svys <- lapply(lapply(clean_data, names), as.list)
+  # Pip data cleaned
+
+  svys <- lapply(lapply(clean_data, names), as.list) # temporary
   svys <- Filter(function(y) !(is.list(y) && length(y) == 0), svys)
 
   pip_inv <- data.frame(
     survey_id = rep(names(svys), lengths(svys)),
     pip_id = unlist(svys, use.names = FALSE)
   )
+
+  # Bind versions
 
   vrs_dt <- data.table::rbindlist(pins_versions_data, idcol = "pip_id")
 
@@ -24,6 +28,8 @@ update_pip_inventory <- function(inv_to_clean,
                     suffix = c("_data","_metadata"),
                     reportvar = FALSE,
                     verbose = FALSE)
+
+  # Add info from DLW inventory
 
   pip_inv <- pip_inv |>
     joyn::left_join(vrs, by = "pip_id",
@@ -36,23 +42,51 @@ update_pip_inventory <- function(inv_to_clean,
     collapse::frename(version = "version_dlw",
                       created = "created_dlw",
                       hash    = "hash_dlw",
-                      pins_folder = "dlw_folder")
+                      pins_folder = "pins_dlw")
 
+  # Save master inventory
 
-  board <- pipfun::get_pins_boards(board = "pip_master_inventory")
+  old_pip_inv <- pipload::load_pip_master_inventory()
 
-  # inv_name <- paste("pip_inv_",format(Sys.time(), "%Y%m%d"), sep = "")
+  new_pip_inv <- pip_inv|>
+    collapse::rowbind(old_pip_inv)|>
+    collapse::funique()
 
-  pins::pin_write(board                 = board,
-                   x                     = pip_inv,
+  board_master <- pipfun::get_pins_boards(board = "pip_master_inventory")
+
+  pins::pin_write(board                 = board_master,
+                   x                     = new_pip_inv,
                    name                  = "pip_master_inventory",
                    force_identical_write = FALSE,
                    type                  = "qs",
                   versioned              = TRUE )
 
+  # Save release inventory
+
+  pfw <- pipload::pip_load_aux("pfw", verbose = FALSE)
+
+  pfw_release <- pfw |>
+    collapse::fsubset(inpovcal == 1)|>
+    collapse::fselect(country_code,
+                      surveyid_year,
+                      survey_acronym)
+
+  release_pip_inv  <- new_pip_inv[pfw_release, on = .(country_code,
+                                                      surveyid_year,
+                                                      survey_acronym)]
+
+  board_release <- pipfun::get_pins_boards(board = "pip_release_inventory")
+
+  pins::pin_write(board                 = board_release,
+                  x                     = release_pip_inv,
+                  name                  = "pip_master_inventory",
+                  force_identical_write = FALSE,
+                  type                  = "qs",
+                  versioned              = TRUE )
+
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Return   ---------
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  return(pip_inv)
+  return(new_pip_inv)
 
 }
