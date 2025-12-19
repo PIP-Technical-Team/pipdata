e0654e200a874b9dbe99ce6163b7f2fa4fabc6bb
Tefera19 <tdegefu@worldbank.org>
2025-12-19 08:35:40 -0500

    Update functions to incorporate stamp

--- R/pipdata_get_gmd.R
+++ R/pipdata_get_gmd.R
@@ -1,10 +1,15 @@
-#' Retrieve and Pin GMD Catalog Datasets to a Local Directory
+#' Retrieve and Save GMD Catalog Datasets to a Local Directory
 #'
 #' @description
-#' This wrapper function automates the process of managing GMD catalog datasets by performing the following tasks:
-#' 1. Checks for new datasets in the GMD catalog using the inventory file (`dlw_gmd_inv`), which contains the current list of available GMD datasets.
-#' 2. If new datasets are found (specifically `"GPWG"`, `"GROUP"`, `"BIN"`, `"HIST"`, `"ALL"`, `"ASPIRE"`, and `"L"`), downloads them using `dlw::dlw_get_gmd` and pins them to the local directory.
-#' 3. Updates the inventory file (`dlw_gmd_inv`) with information about the newly downloaded datasets.
+#' This wrapper function automates the process of managing GMD catalog 
+#' datasets by performing the following tasks:
+#' 1. Checks for new datasets in the GMD catalog using the inventory 
+#' file (`dlw_gmd_inv`), which contains the current list of available GMD datasets.
+#' 2. If new datasets are found (specifically `"GPWG"`, `"GROUP"`, 
+#' `"BIN"`, `"HIST"`, `"ALL"`, `"ASPIRE"`, and `"L"`), downloads them 
+#' using `dlw::dlw_get_gmd` and save them to the local directory.
+#' 3. Updates the inventory file (`dlw_gmd_inv`) with information about 
+#' the newly downloaded datasets.
 #'
 #' @param inv_gmd_list Character. The name of the inventory file containing the list of GMD datasets.
 #' @param log Logical. Whether to keep logging information. Default is `TRUE`.
@@ -40,31 +45,32 @@ pipdata_get_gmd <- function(
 
   #### -------------------------------------------------------------------------
 
-  # initialize working release and dlw inventory and data boards
   pipfun::get_wrk_release(verbose = FALSE)
-  dlw_data_board <- pipfun::get_pins_boards(board = "dlw_data")
-  dlw_inv_board  <- pipfun::get_pins_boards(board = "dlw_inventory")
+  pip_folders <- pipfun::get_pip_folders()
 
-  # check directory existence for inventory and data boards
-  check_directory(dlw_data_board)
-  check_directory(dlw_inv_board)
+  dlw_data <- pip_folders$dlw_data
+  dlw_inv  <- pip_folders$dlw_inventory
+
+  # check directory existence for inventory and dlw data
+  check_directory(dlw_data)
+  check_directory(dlw_inv)
 
   ### -------------------------------------------------------------------------
 
   # 1) check if there is any new GMD datasets
 
-  inv_gmd <- dlw_gmd_new(inv_gmd_list = inv_gmd_list,
-                             check_missing = check_missing)
+  inv_gmd <- dlw_gmd_new(check_missing = check_missing)
 
   if (is.null(inv_gmd) || nrow(inv_gmd) == 0) cli::cli_abort("There is no new data on GMD catalog")
 
   # 2) get the data from GMD catalog and pin to local folder -------------------
 
-  cli::cli_alert_info("pin folder: {.dir {dlw_data_board$path}}")
+  cli::cli_alert_info("Working folder: {.dir {dlw_data}}")
 
   cli::cli_progress_bar("Downloading .qs", total = nrow(inv_gmd))
 
   inv_gmd$data_available <- NA
+  inv_gmd <- inv_gmd[c(1:4),]
   for (i in seq_along(1:nrow(inv_gmd))){
 
     # extract relevant information for the current row to get the data
@@ -81,11 +87,11 @@ pipdata_get_gmd <- function(
         dlw::dlw_get_gmd(
           country_code = country,
           year = year,
-          # survey = svy_acronym, #
+          survey = svy_acronym, 
           module = md_type,
-          # vermast = vermst,  #
-          # veralt = veralt,  #
-          local_dir = dlw_data_board$path
+          vermast = vermst,  
+          veralt = veralt,  
+          local_dir = dlw_data
         )
 
         # Mark data as available if download is successful
@@ -109,7 +115,7 @@ pipdata_get_gmd <- function(
                                       module       = md_type,
                                       vermast      = vermst,
                                       veralt       = veralt,
-                                      local_dir    = dlw_data_board$path),
+                                      local_dir    = dlw_data),
                           logmeta = list(error = e))
         }
 
@@ -131,7 +137,7 @@ pipdata_get_gmd <- function(
 
   # 3) save the GMD list -------------------------------------------------------
 
-  # get list of datasets already pinned in the local folder
+  # get list of datasets already saved in the local folder
   inv_gmd_match <- dlw_gmd_match()
 
   if (!is.null(inv_gmd_match)){
@@ -139,17 +145,23 @@ pipdata_get_gmd <- function(
     inv_gmd <- rbind(inv_gmd, inv_gmd_match, ignore.attr=TRUE, fill = TRUE)
   }
 
-  dlw_inv_board |>
-    pipload::pip_write(inv_gmd, inv_gmd_list)
+  stamp::st_init(dlw_inv)
+  # dlw_inv |>
+    # pipload::pip_write(inv_gmd, inv_gmd_list)
+
+  pipload::pip_write(x = inv_gmd,
+    id = inv_gmd_list,
+    dir = dlw_inv,
+    format  = "qs2")
 
 
-  cli::cli_alert_success("GMD inventory file is saved at: {.dir {dlw_inv_board$path}}")
+  cli::cli_alert_success("GMD inventory file is saved at: {.dir {dlw_inv}}")
 
   if (log) {
 
     pipfun::log_add("info", "Inventory file is saved",
                     name = "pipdata_log",
-                    logmeta = list(saved_at = dlw_inv_board$path))
+                    logmeta = list(saved_at = dlw_inv))
   }
 
   # 4) save the logging file ---------------------------------------------------
@@ -160,7 +172,7 @@ pipdata_get_gmd <- function(
 
     pipfun::log_add("info", "logging file is saved",
                     name = "pipdata_log",
-                    logmeta = list(log_path = "dlw_gmd_log"))
+                    logmeta = list(log_file = "dlw_gmd_log"))
 
     cli::cli_alert_success("GMD logging file is saved")
 
